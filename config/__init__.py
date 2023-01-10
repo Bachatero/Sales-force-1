5c977d7c2f5093f4b5f43c7617cc4d8888a0db90
Bachatero <oracle@Bachatero.local>
2023-01-10 15:38:38 +0100

    add stuff

---
+++ config/__init__.py
@@ -0,0 +1,3 @@
+outputDir = r"D:\MyReporting\OutputFiles\Salesforce"
+logFile = "D:\MyReporting\Python_Scripty\Salesforce\log\salesforce.log"
+loginConfig="D:\MyReporting\Python_Scripty\Salesforce\config\loginConfig.json"
